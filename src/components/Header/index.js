7b9d825d16794ccd2fefaba11e8c03c37589b285
kallagallasubbu <kallagallasubbu@gmail.com>
2025-04-23 09:54:40 +0000

    update Code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,27 +15,27 @@ const Header = props => {
   }
 
   const renderCartIcon = () => (
-    <div className="cart-container">
-      <Link to="/cart">
-        <button type="button" className="cart-button" data-testid="cart">
-          <AiOutlineShoppingCart className="cart-icon" />
+    <div className='cart-container'>
+      <Link to='/cart'>
+        <button type='button' className='cart-button' data-testid='cart'>
+          <AiOutlineShoppingCart className='cart-icon' />
         </button>
       </Link>
-      <div className="cart-count-badge">
-        <p className="cart-count">{cartList.length}</p>
+      <div className='cart-count-badge'>
+        <p className='cart-count'>{cartList.length}</p>
       </div>
     </div>
   )
 
   return (
-    <header className="nav-header">
-      <Link to="/">
-        <h1 className="logo">{restaurantName}</h1>
+    <header className='nav-header'>
+      <Link to='/'>
+        <h1 className='logo'>{restaurantName}</h1>
       </Link>
-      <div className="nav-items">
-        <p className="orders-text">My Orders</p>
+      <div className='nav-items'>
+        <p className='orders-text'>My Orders</p>
         {renderCartIcon()}
-        <button type="button" className="logout-button" onClick={onLogout}>
+        <button type='button' className='logout-button' onClick={onLogout}>
           Logout
         </button>
       </div>
